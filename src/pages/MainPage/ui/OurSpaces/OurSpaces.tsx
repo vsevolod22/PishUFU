b114974ea2a743328f76cc62c718f54b4db858a9
vsevolod22 <240303vilkov@gmail.com>
2024-07-10 23:20:07 +0300

    add one more section

--- src/pages/MainPage/ui/OurSpaces/OurSpaces.tsx
+++ src/pages/MainPage/ui/OurSpaces/OurSpaces.tsx
@@ -1,6 +1,7 @@
 
 import {classNames} from "shared/lib/classNames/classNames";
 import styles from './OurSpaces.module.scss'
+import arrow from 'shared/assets/icons/arrow.svg'
 import firstBig from 'shared/assets/backgroundImg/OurSpaces/firstBig.png'
 import secondBig from 'shared/assets/backgroundImg/OurSpaces/SecondBig.png'
 import thirdBig from 'shared/assets/backgroundImg/OurSpaces/ThirdBig.png'
@@ -29,16 +30,21 @@ const OurSpaces = ({className} : OurSpacesProps) => {
             <div className={styles.ourSpaces__container}>
                 <div className={styles.ourSpaces_card__container}>
                     <div className={styles.ourSpaces_card}>
-                    <div className={styles.ourSpaces_card_img__container}>
-                        <div className={styles.ourSpaces_card_img__big}>
-                            <img src={firstBig} alt="Образовательные пространства"/>
+                        <div className={styles.ourSpaces_card_img__container}>
+                            <div className={styles.ourSpaces_card_img__big}>
+                                <img src={firstBig} alt="Образовательные пространства"/>
+                            </div>
+                            <div className={styles.ourSpaces_card_img__small}>
+                                <img src={firstSmall} alt="Образовательные пространства"/>
+                                <img src={firstSmallSecond} alt="Образовательные пространства"/>
+                            </div>
                         </div>
-                        <div className={styles.ourSpaces_card_img__small}>
-                            <img src={firstSmall} alt="Образовательные пространства"/>
-                            <img src={firstSmallSecond} alt="Образовательные пространства"/>
+                        <div className={styles.ourSpaces_card_switch__container}>
+                            <span>Образовательные пространства<img src={arrow} alt="arrow"/></span>
+                            <span>02<img src={arrow} alt="arrow"/></span>
+                            <span>03<img src={arrow} alt="arrow"/></span>
                         </div>
-                    </div>
-                        
+
 
                     </div>
                 </div>
